943d95fad46632e964068ab5a4ac0b60fd0a4fb3
alicarogojin <45542215+alicarogojin@users.noreply.github.com>
2023-11-29 16:07:50 -0500

    Update create_spheres.sh

--- make_headmodel/create_spheres.sh
+++ make_headmodel/create_spheres.sh
@@ -1,42 +1,18 @@
 #!/bin/tcsh
+# These commands are applied to MEG dataset
+# To make a head model, we have to do localspheres on each MEG dataset, incorporating the head position information.
+# See the NIH webpage for details.
+set mri = ${workdir}/anat
+set ds = ${exp_folder}/GRAND_DS/${subj}/${subj}_grandDS.ds
 
-set this_dir = `dirname $0`
-source ${this_dir}/../common_cli_vars/get_participant_id.sh
-if ($status != 0) then
-    echo "Unable to get participant ID from your interactive input"
-    echo "Please try again. Bye!"
-endif
+localSpheres -d $ds -s $mri/ortho.shape -M -v > ${ds}/sphereinfo.txt
+checkSpheres $ds >> ${ds}/sphereinfo.txt
+inflateSpheres $ds >> ${ds}/sphereinfo.txt
 
-set subj=${participant_id}
+# Check that there are no errors or warnings
+head ${ds}/sphereinfo.txt
+tail ${ds}/sphereinfo.txt
 
-# get data_dir
-source ${this_dir}/../common_cli_vars/path_vars.sh
-if ($?data_dir) then
-    echo "Please define data_dir in picture_naming_code/common_cli_vars/path_vars.sh"
-    echo "and try again. Bye!"
-    exit 4
-endif
-
-set subj_data_dir=${data_dir}/proc/${subj}
-set anatdir = ${data_dir}/proc/${subj}/anat/
-
-if (! -d ${anatdir}) then
-    echo "working directory does not exist: ${anatdir}."
-    echo " Are you sure you have the correct participant ID and 3dcopy was successful?" 
-    echo "Fix it and try again. Bye!"
-    exit 3
-endif
-
-#:'these commands are applied to MEG dataset'
-#:To make a head model, we have to do localspheres on each MEG dataset, incorporating the head position information.
-#:See the NIH webpage for details.
-
-set mri = ${anatdir}
-set dspath = ${subj_data_dir}
-
-foreach run (PWI overt beginMatch)
-    set ds = ${dspath}/pic_onset_${run}.ds
-    localSpheres -d $ds -s $mri/ortho.shape -M -v > ${ds}/sphereinfo.txt
-    checkSpheres $ds >> ${ds}/sphereinfo.txt
-    inflateSpheres $ds >> ${ds}/sphereinfo.txt
-end
+# Copy everything over to the ${exp_folder}/MRI directory to have as a backup
+scp -r ${workdir}/anat /auto/iduna/arogojin/bilateral_squeeze_test/MRI/${subj}
+scp -r ${workdir}/samresults /auto/iduna/arogojin/bilateral_squeeze_test/MRI/${subj}
