e94f4c39794cff2fe67b434662739d1679f29ce7
alicarogojin <45542215+alicarogojin@users.noreply.github.com>
2023-11-29 15:58:26 -0500

    Update create_brainhull.sh

--- make_headmodel/create_brainhull.sh
+++ make_headmodel/create_brainhull.sh
@@ -1,47 +1,23 @@
 #!/bin/tcsh
+# Need Brainhull package for
+	# Warping MRI image to ORTHO Space
+	# Skull stripping
+	# Approximating inner skull surface
 
-set this_dir = `dirname $0`
-source ${this_dir}/../common_cli_vars/get_participant_id.sh
-if ($status != 0) then
-    echo "Unable to get participant ID from your interactive input"
-    echo "Please try again. Bye!"
-endif
-
-set subj=${participant_id}
-
-# get data_dir
-source ${this_dir}/../common_cli_vars/path_vars.sh
-if ($?data_dir) then
-    echo "Please define data_dir in picture_naming_code/common_cli_vars/path_vars.sh"
-    echo "and try again. Bye!"
-    exit 4
-endif
-
-set anatdir = $data_dir/proc/${subj}/anat/
-
-if (! -d ${anatdir}) then
-    echo "working directory does not exist: ${anatdir}."
-    echo " Are you sure you have the correct participant ID and 3dcopy was successful?" 
-    echo "Fix it and try again. Bye!"
-    exit 3
-endif
-
-cd $anatdir
-
-#'we set libdir just to say where the code lives for the brainhull package, '
-#'then use 3dTagalign to "rotate" the person's original MRI into the coordinate space of CTF, defined by the three fiducial points. '
-#'The resulting brain, called "ortho" is quite slanted. '
-
+# We set libdir just to say where the code lives for the brainhull package
 set libdir = /home/jed/data/sw/brainhulllib
+
+# Warping into ortho space
+# Use 3dTagalign to "rotate" the person's original MRI into the coordinate space of CTF, defined by the three fiducial points
+# The resulting brain, called "ortho" is quite slanted
+cd ${workdir}/anat
 3dTagalign -matvec orthomat -prefix ./ortho -master $libdir/master+orig mprage+orig
 
-#:'This line is necessary for AFNI-internal reasons. '
-
+# This line is necessary for AFNI-internal reasons
 3drefit -markers ortho+orig
 
-#:' Strip the skull off. This command takes time'
-
-3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes 
+#Strip the skull off - this command takes time
+3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes
 
 # with `-mask-vol`, each pixel has one of the following values:
 # 0: Voxel outside surface
@@ -53,19 +29,23 @@ set libdir = /home/jed/data/sw/brainhulllib
 # 3: Voxel contains a surface node.
 # etc...
 
-#:'Skull stripping usually works well, but here are some options to try if it doesn't. '
-#:3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -ld 30 -blur_fwhm 2 -visual -shrink_fac 0.5 -init_radius 75
-#:3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -push_to_edge -ld 30 -use_edge -blur_fwhm 2 -visual -shrink_fac 0.5 -init_radius 70
-#:3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -push_to_edge
+# Skull stripping usually works well, but here are some options to try if it doesn't
+# 3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -ld 30 -blur_fwhm 2 -visual -shrink_fac 0.5 -init_radius 75
+# 3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -push_to_edge -ld 30 -use_edge -blur_fwhm 2 -visual -shrink_fac 0.5 -init_radius 70
+# 3dSkullStrip -input ortho+orig -prefix mask -mask_vol -no_avoid_eyes -push_to_edge
 
+# This is the brainhull procedure documented on the NIH webpage
+# Construct the brain hull
+# hull2fid program gives error so we are using hull2fid_2023 instead
 
-#:'This is the brainhull procedure documented on the NIH webpage. '
-#:'hull2fid program gives error so we are using hull2fid_2023 instead
-# step(b - 2.9) -> returns 0 iff b < 3
 3dcalc -a ortho+orig -b mask+orig -prefix brain -expr 'a * step(b - 2.9)'
+
 brainhull mask+orig > hull
 hull2fid_2023 ortho+orig hull ortho
-
 hull2suma hull
+
+# Visualize the hull
 suma -novolreg -spec hull.spec &
+
+# Should see all three markers in the same slice
 afni -niml -dset ortho+orig
