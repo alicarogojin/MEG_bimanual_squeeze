29d22c797acde5edcedd9f64e7495abac1bb8acb
alicarogojin <45542215+alicarogojin@users.noreply.github.com>
2023-11-29 16:09:33 -0500

    Update create_spheres.sh

--- make_headmodel/create_spheres.sh
+++ make_headmodel/create_spheres.sh
@@ -2,6 +2,21 @@
 # These commands are applied to MEG dataset
 # To make a head model, we have to do localspheres on each MEG dataset, incorporating the head position information.
 # See the NIH webpage for details.
+
+# Set relevant dataset information to be preprocessed
+echo "Enter the participant ID: "
+set subj = "$<"
+
+# If the subject directory in PROC doesn't exist, create it
+if ( -e /auto/iduna/arogojin/bilateral_squeeze_test/PROC/${subj} ) then
+   echo "Directory PROC/${subj} exists"
+else
+   mkdir /auto/iduna/arogojin/bilateral_squeeze_test/PROC/${subj}
+   echo "Directory PROC/${subj} created"
+endif
+
+set exp_folder = '/auto/iduna/arogojin/bilateral_squeeze_test'
+set workdir = ${exp_folder}/PROC/${subj}
 set mri = ${workdir}/anat
 set ds = ${exp_folder}/GRAND_DS/${subj}/${subj}_grandDS.ds
 
