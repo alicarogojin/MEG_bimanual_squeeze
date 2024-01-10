2b6604a6671f3f9a8c937ae551bbecef04c855d8
alicarogojin <45542215+alicarogojin@users.noreply.github.com>
2024-01-10 13:50:21 -0500

    Update and rename test_transfer_code.sh to transfer_code.sh

--- transfer_code.sh
+++ transfer_code.sh
@@ -2,10 +2,10 @@
 # Transfer data from MEG to lab server
 
 # Set relevant code file name to be transferred
-echo "Enter the code path and filename (e.g., active_code/test_addMarkers.sh)"
+echo "Enter the code path and filename (e.g., active_code/addMarkers.sh)"
 set filename = "$<"
 
-set exp_folder = /auto/iduna/arogojin/bilateral_squeeze_test
+set exp_folder = /rri_disks/eugenia/meltzer_lab/bilateral_squeeze
 set work_dir = ${exp_folder}/code
 
 # Copy code files from local computer to magneto server
